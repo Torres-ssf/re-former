818522e65d36e77739dd25f9495957485d89901d
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-06-14 01:23:48 +0100

    Complete edit form for updating user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,36 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if User.update(user_params)
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    def create
-        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-        @user = User.new(user_params)
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
-        if @user.save
-            redirect_to new_user_path
-        else
-            render :new
-        end
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    private
+  private
 
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
